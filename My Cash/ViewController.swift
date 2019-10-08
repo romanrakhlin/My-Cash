84e736cf686ef4384bc427ffa51ba12386ee2e3a
romarakhlin <rrakhlin@gmail.com>
2019-10-08 15:03:41 +0300

    Initial Commit

---
+++ My Cash/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My Cash
+//
+//  Created by Roman Rakhlin on 08.10.2019.
+//  Copyright © 2019 Roman Rakhlin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
