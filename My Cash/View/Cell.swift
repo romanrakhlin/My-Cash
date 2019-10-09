412882956b8722d69d1767b36a41bb7d6e030f5a
romarakhlin <rrakhlin@gmail.com>
2019-10-10 01:05:02 +0300

    first commit

---
+++ My Cash/View/Cell.swift
@@ -0,0 +1,24 @@
+//
+//  Cell.swift
+//  My Cash
+//
+//  Created by Roman Rakhlin on 08.10.2019.
+//  Copyright © 2019 Roman Rakhlin. All rights reserved.
+//
+
+import UIKit
+
+class Cell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ My Cash/View/TextFieldView.swift
@@ -0,0 +1,21 @@
+//
+//  TextFieldView.swift
+//  My Cash
+//
+//  Created by Roman Rakhlin on 08.10.2019.
+//  Copyright © 2019 Roman Rakhlin. All rights reserved.
+//
+
+import UIKit
+
+class TextFieldView: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
